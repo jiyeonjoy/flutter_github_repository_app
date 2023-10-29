d1d28a744c1f4f86e3d0f0284c93903d770a8084
최지연 <choi02647@naver.com>
2023-10-30 01:04:09 +0900

    SavedPage 유아이 추가

--- lib/app/saved/controller/saved_page_controller.dart
+++ lib/app/saved/controller/saved_page_controller.dart
@@ -1,6 +1,8 @@
+import 'package:flutter_github_repository_app/data/dto/response/search_repos/search_repos_item_dto.dart';
 import 'package:get/get.dart';
 
 class SavedPageController extends GetxController {
   static SavedPageController get to => Get.find();
 
+  var savedList = <SearchReposItemDto>[].obs;
 }
--- lib/app/saved/ui/saved_page.dart
+++ lib/app/saved/ui/saved_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_github_repository_app/app/common/config/r.dart';
+import 'package:flutter_github_repository_app/app/common/ui/github_repository_item_view.dart';
 import 'package:flutter_github_repository_app/app/saved/controller/saved_page_controller.dart';
+import 'package:flutter_github_repository_app/app/saved/ui/empty_view.dart';
+import 'package:flutter_github_repository_app/data/dto/response/search_repos/search_repos_item_dto.dart';
 import 'package:get/get.dart';
 
 class SavedPage extends GetView<SavedPageController> {
@@ -7,12 +11,31 @@ class SavedPage extends GetView<SavedPageController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
-        child: Text(
-          'Saved Page',
-        ),
+    return Scaffold(
+      backgroundColor: R.color.white,
+      body: SafeArea(
+        child: Obx(() {
+          List<SearchReposItemDto> savedList = controller.savedList;
+          if (savedList.isEmpty) {
+            return EmptyView();
+          } else {
+            return ListView.builder(
+              itemBuilder: (_, index) {
+                return GitHubRepositoryItemView(
+                  savedList[index],
+                  IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: R.color.color_F91E30,
+                    ),
+                    onPressed: () {},
+                  ),
+                );
+              },
+              itemCount: savedList.length,
+            );
+          }
+        }),
       ),
     );
   }
